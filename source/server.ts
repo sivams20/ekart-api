4d7369fef404f128d4a5d48e3d913eeff3b7dfb1
sivams20 <sivams20@gmail.com>
2021-08-26 09:17:22 +0530

    Added route for products and orders

--- source/server.ts
+++ source/server.ts
@@ -1,6 +1,7 @@
 
 import http from "http";
 import express from "express";
+//import bodyParser from "body-parser";
 
 import getAllUsersController from './controllers/getAllUsersController';
 import productRoute from './api/routes/products';
@@ -12,8 +13,11 @@ const app = express();
 const server = new http.Server(app);
 server.listen(3000);
 
-app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/products', productRoute);
 app.use('/orders', orderRoute);
